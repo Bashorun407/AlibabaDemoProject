9473684c529b6e2627a09d4f09b421977f15624b
Akinbobola Oluwaseyi <boboladvat@gmail.com>
2021-09-23 11:27:48 +0100

    newly added ProductReviewApi

--- src/main/java/com/alibabademo/alibaba/Controller/ProductReviewApi.java
+++ src/main/java/com/alibabademo/alibaba/Controller/ProductReviewApi.java
@@ -1,4 +1,24 @@
 package com.alibabademo.alibaba.Controller;
 
+import com.alibabademo.alibaba.Dao.ProductDto;
+import com.alibabademo.alibaba.Entity.ProductReview;
+import com.alibabademo.alibaba.RestResponse.ResponsePojo;
+import com.alibabademo.alibaba.Service.ProductReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/alibaba")
 public class ProductReviewApi {
-}
+
+    @Autowired
+    private ProductReviewService productReviewService;
+
+    //(1) Method 1 for Creating a ProductReview
+    public ResponsePojo<ProductReview> updateRatingsAndReviews(ProductDto productDto, Long rating){
+        return productReviewService.updateRatingsAndReviews(productDto, rating);
+    }
+
+
+    }
